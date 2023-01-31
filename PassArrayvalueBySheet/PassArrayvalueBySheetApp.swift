9b8a6e8d6810f39ff97b2ac8f638df7f79619c42
sasasan03 <sako.hiro62@gmail.com>
2023-01-31 18:51:01 +0900

    Initial Commit

---
+++ PassArrayvalueBySheet/PassArrayvalueBySheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassArrayvalueBySheetApp.swift
+//  PassArrayvalueBySheet
+//
+//  Created by sako0602 on 2023/01/31.
+//
+
+import SwiftUI
+
+@main
+struct PassArrayvalueBySheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
